2082131d6f3db928351ec50c6d156314af33ba38
Lucas Caribé <lucasmoc47@gmail.com>
2021-11-09 04:56:50 -0300

    Adding post view, serializer and permissions

---
+++ posts/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+
+class PostPermissions(permissions.BasePermission):
+  def has_permission(self, request, view):
+    if view.action == 'list' or view.action == 'retrieve':
+      return True
+    elif view.action == 'create':
+      return request.user.is_authenticated
+    else:
+      return False
+
+  def has_object_permission(self, request, view, obj):
+    if not request.user.is_authenticated:
+      return False
+    
+    if view.action == 'retrieve':
+      return True
+    elif view.action in ['update', 'partial_update', 'destroy']:
+      return obj.owner == request.user or request.user.is_admin
+    else:
+      return False
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import PostSerializer
+from .models import Post
+from .permissions import PostPermissions
+
+class PostView(viewsets.ModelViewSet):
+  serializer_class = PostSerializer
+  model = Post
+  queryset = Post.objects.all()
+  permission_classes = (PostPermissions,)
